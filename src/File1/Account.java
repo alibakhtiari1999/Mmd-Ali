9d12cbb3fcd20cfabf628dc535bf7382041e8784
MOhammad <heidary13794@gmail.com>
2019-04-14 22:34:01 +0430

    MMd 3

---
+++ src/File1/Account.java
@@ -0,0 +1,71 @@
+package File1;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Account {
+    private String state;
+    private static Scanner scanner=new Scanner(System.in);
+    private static Pattern pattern;
+    private static Matcher matcher;
+    private static String createAccount ="^create account ([^ ]+)$";
+    private static String login ="^login ([^ ]+)$";
+    //private static String createAccount ="^create account ([^ ]+)$";
+
+
+
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    public static String AccountMenu(){
+        while (true){
+            String commandTxt=scanner.nextLine();
+            //create account
+            pattern = Pattern.compile(createAccount);
+            matcher = pattern.matcher(commandTxt);
+            if (matcher.find()){
+                createAccount(matcher.group(1));
+                continue;
+            }
+            //login
+            pattern = Pattern.compile(login);
+            matcher = pattern.matcher(commandTxt);
+            if (matcher.find()){
+                String userName=matcher.group(1);
+                if (!checkUserValidity(userName)){
+                    System.out.println("Invalid userName...");
+                    continue;
+                }
+                login(userName);
+            }
+
+            pattern = Pattern.compile(createAccount);
+            matcher = pattern.matcher(commandTxt);
+            if (matcher.find()){
+                createAccount(matcher.group(1));
+
+            }
+            System.out.println("Invalid command!");
+
+        }
+
+
+    }
+    private static void createAccount (String userName){
+
+    }
+
+    private static boolean login (String userName){
+    //check password
+
+        return true;
+    }
+
+    private static boolean checkUserValidity(String username){
+
+        return true;
+    }
+
+}
--- src/File1/Main.java
+++ src/File1/Main.java
@@ -3,6 +3,6 @@ package File1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }

1e6b2d71e8c2ff774d8023bbd474b2481ee7546c
alibakhtiyari9@yahoo.com <Optimom110>
2019-04-15 16:21:56 +0430

    commit 6 ali

--- src/File1/Board.java
+++ src/File1/Board.java
@@ -1,25 +1,23 @@
 package File1;
 
 public class Board {
-
     private static Board board;
-
     private static Location gamePlane[][] = new Location[9][5];
 
+
+
     private Board(){
-        if (board==null){
-
-            for (int i = 0; i < 9; i++){
-                for (int j = 0; j < 5; j++){
-                    gamePlane[i][j] = new Location();
-                }
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 5; j++){
+                gamePlane[i][j] = new Location();
             }
-
         }
     }
-
-    public static getInstance(){
-
+    public static Board getInstance(){
+        if (board==null){
+            board = new Board();
+        }
+        return board;
     }
 
 }
--- src/File1/Location.java
+++ src/File1/Location.java
@@ -1,26 +1,33 @@
 package File1;
 
 public class Location {
-
     private static int numberOfLocations = 0;
-
     private boolean isFull = false;
-    private static Location loc;
+    private Location loc;
+    private Card cardOnLocation;
+    private Spell spell;
 
+    public Card getCardOnLocation() {
+        return cardOnLocation;
+    }
+    public void setCardOnLocation(Card cardOnLocation) {
+        if (cardOnLocation==null){
+            this.cardOnLocation = cardOnLocation;
+            System.out.println("card sucsessfully put in cell");
+        }
+        System.out.println("there is already a card in this cell, try another cell. ");
+    }
     private Location() {
         if (numberOfLocations < 45){
             loc = new Location();
         }
     }
-
-    public static Location getInstance(){
+    public Location getInstance(){
         return loc;
     }
-
     public boolean isFull() {
         return isFull;
     }
-
     public void setFull(boolean full) {
         isFull = full;
     }
